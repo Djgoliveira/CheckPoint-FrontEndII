68616a5ba378295c1bf17b34f4de089515779759
Douglas Oliveira <109635136+Djgoliveira@users.noreply.github.com>
2023-03-13 16:38:58 -0300

    Update codigo.js
    Atualização de Estilos

--- javaScript/codigo.js
+++ javaScript/codigo.js
@@ -1,4 +1,3 @@
-
 //Vamos criar agora a primeira Div para colocar os elementos dentro
 //criando a div
 let elementoPai = document.getElementById("ElementoPai");
@@ -11,41 +10,60 @@ function PontosTuristicos(imageUrl, cidade, pontoTuristico, descricao) {
     this.descricao = descricao;
 }
 
+let btnCadastrar = document.getElementById("btn");
+btnCadastrar.addEventListener("click", function(){
+    Cadrastrar();
+});
+
 let listaPontoTuristicos =[];
 
 function Cadrastrar(){
 
-    const a = document.getElementById("url_cad");
-    const b = document.getElementById("nome_cad");
-    const c = document.getElementById("NomeLocal_cad");
-    const d = document.getElementById("descricao_cad");
+    const imgUrl = document.getElementById("url_cad");
+    const nomeCidade = document.getElementById("nome_cad");
+    const pontoTuristico = document.getElementById("NomeLocal_cad");
+    const descricaoCidade = document.getElementById("descricao_cad");
 
-    const n = new PontosTuristicos(a.value ,b.value ,c.value ,d.value)
-    listaPontoTuristicos.push(n);   
-    rendenizarCards();
-    console.log(listaPontoTuristicos);
+    const n = new PontosTuristicos(imgUrl.value,nomeCidade.value ,pontoTuristico.value,descricaoCidade.value);
+    if (nomeCidade.value == 0 ) { 
+        alert(`você não preencheu o nome da Cidade`);
+    }else if (pontoTuristico.value == 0 ) { 
+        alert(`você não preencheu o nome do local`);
+    }else if (imgUrl.value == 0 ) { 
+        alert(`você não preencheu a Url da Imagem`);
+    }else if (descricaoCidade.value == 0 ) { 
+        alert(`você não preencheu a descrição`);
+    }    else {
+        listaPontoTuristicos.push(n);   
+        rendenizarCards(); 
+    }
+
     
+       
 };
+
+
 
 function rendenizarCards(){
 
-    // percorrer a lista de ponto turisticos
-    for (const card of listaPontoTuristicos) {
-        
-        let novaDiv = document.createElement("div");
+    let novaDiv = document.createElement("div");
         novaDiv.setAttribute("class", "cards");
 
-            novaDiv.innerHTML = `
-                                    <img src="${card.imageUrl}" class = "card-img">
-                                    <h1 class ="titulos">${card.cidade}</h1>
-                                    <h2 class ="titulos">${card.pontoTuristico}</h2>
-                                    <p class ="descricao">
+    // percorrer a lista de ponto turisticos
+    for (const card of listaPontoTuristicos) {        
+        
+
+            novaDiv.innerHTML = `   <div class="card-filme">                              
+                                    <h1 class ="titulos-card">${card.cidade}</h1>
+                                    <h2 class ="titulos-card">${card.pontoTuristico}</h2>
+                                    <img src="${card.imageUrl}" class ="card">
+                                    <p class ="texto-img">
                                     ${card.descricao}
                                     </p>
+                                    </div>
                                 `;
         elementoPai.appendChild(novaDiv);
     }
 }
 
 
-console.log(listaPontoTuristicos);
