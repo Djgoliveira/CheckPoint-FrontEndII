352d7efc0eaa40af585b3056c34f28208032f1b4
GustavoFlorenci0 <florencio152@gmail.com>
2023-03-15 19:03:21 -0300

    ajustando a validação do botao de submit

--- javaScript/codigo.js
+++ javaScript/codigo.js
@@ -1,38 +1,51 @@
 //Vamos criar agora a primeira Div para colocar os elementos dentro
-//criando a div
 
 let elementoPai = document.getElementById("ElementoPai");
 elementoPai.classList.add('banner-cards');
 
+
+// Criando um construtor para utilizar o cadastro.
+
 function PontosTuristicos(imageUrl, cidade, pontoTuristico, descricao) {
     this.imageUrl = imageUrl;
     this.cidade = cidade;
-    this. pontoTuristico = pontoTuristico;
+    this.pontoTuristico = pontoTuristico;
     this.descricao = descricao;
 }
 
-let listaPontoTuristicos =[];
+// criando a lista.
 
-function Cadrastrar(){
+let listaPontoTuristicos = [];
+
+// criando a função de cadastrar;
+
+function Cadrastrar() {
 
     const imgUrl = document.getElementById("url_cad");
     const nomeCidade = document.getElementById("nome_cad");
     const pontoTuristico = document.getElementById("NomeLocal_cad");
     const descricaoCidade = document.getElementById("descricao_cad");
 
-    const n = new PontosTuristicos(imgUrl.value,nomeCidade.value ,pontoTuristico.value,descricaoCidade.value);
-    if (nomeCidade.value && pontoTuristico.value && imgUrl.value && descricaoCidade.value) { 
-        listaPontoTuristicos.push(n);   
-        rendenizarCards();    
+    const n = new PontosTuristicos(imgUrl.value, nomeCidade.value, pontoTuristico.value, descricaoCidade.value);
+    if (nomeCidade.value && pontoTuristico.value && imgUrl.value && descricaoCidade.value) {
+        listaPontoTuristicos.push(n);
+        rendenizarCards();        
     }
+    imgUrl.value = "";
+    nomeCidade.value = "";
+    pontoTuristico.value = "";
+    descricaoCidade.value = "";
+    
 };
 
-function rendenizarCards(){
+// criando a função para renderizar os cards;
+
+function rendenizarCards() {
     let novaDiv = document.createElement("div");
-        novaDiv.setAttribute("class", "cards");
+    novaDiv.setAttribute("class", "cards");
     // percorrer a lista de ponto turisticos
-    for (const card of listaPontoTuristicos) {     
-                    novaDiv.innerHTML = `   <div class="card-filme">                              
+    for (const card of listaPontoTuristicos) {
+        novaDiv.innerHTML = `   <div class="card-filme">                              
                                     <h1 class ="titulos-card">${card.cidade}</h1>
                                     <h2 class ="titulos-card">${card.pontoTuristico}</h2>
                                     <img src="${card.imageUrl}" class ="card">
--- javaScript/validacoes.js
+++ javaScript/validacoes.js
@@ -1,102 +1,61 @@
+// Buscando os itens para serem manipulados;
+
 const btnValidacao = document.getElementById("btn");
-btnValidacao.style.backgroundColor = "gray";
-
 const cidadeValidacao = document.getElementById("nome_cad");
-
 const pontoTuristicoValidacao = document.getElementById("NomeLocal_cad");
-pontoTuristicoValidacao.setAttribute("disabled", true);
-
 const imgValidacao = document.getElementById("url_cad");
-imgValidacao.setAttribute("disabled", true);
-
 const descricaoCidadeValidacao = document.getElementById("descricao_cad");
-descricaoCidadeValidacao.setAttribute("disabled", true);
 
-const recarregarCampos = document.getElementById('btn2');
-recarregarCampos.style.backgroundColor = "gray";
-recarregarCampos.setAttribute("disabled", true);
+// criando as validaçoes para cada input;
 
-cidadeValidacao.addEventListener("keyup", function(){
+cidadeValidacao.addEventListener("keyup", function () {
     let validacao = document.getElementById("smallCidade");
     if (cidadeValidacao.value.length >= 4) {
         validacao.innerText = "Ok!";
-        validacao.style.color="green"; 
-        pontoTuristicoValidacao.removeAttribute("disabled"); 
-                                 
+        validacao.style.color = "green";
+
     } else {
         cidadeValidacao.style.backgroundColor = "#ffffff";
         validacao.innerText = "Campo obrigatorio com 4 letras no minimo";
-        validacao.style.color="red"; 
-        btnValidacao.setAttribute("disabled", true);        
+        validacao.style.color = "red";
     }
 })
 
-pontoTuristicoValidacao.addEventListener("keyup", function(){
+pontoTuristicoValidacao.addEventListener("keyup", function () {
     let validacao = document.getElementById("localSmall");
     if (pontoTuristicoValidacao.value.length >= 4) {
         validacao.innerText = "Ok!";
-        validacao.style.color="green";
-        imgValidacao.removeAttribute("disabled");
-             
+        validacao.style.color = "green";
+
     } else {
         pontoTuristicoValidacao.style.backgroundColor = "#ffffff";
         validacao.innerText = "Campo obrigatorio com 4 letras no minimo";
-        validacao.style.color="red";
-        btnValidacao.setAttribute("disabled", true);          
-    }   
+        validacao.style.color = "red";
+
+    }
 })
 
-imgValidacao.addEventListener("keyup", function(){
+imgValidacao.addEventListener("keyup", function () {
     let validacao = document.getElementById("imgSmall");
     if (imgValidacao.value.length >= 10) {
         validacao.innerText = "Ok!";
-        validacao.style.color="green";
-        descricaoCidadeValidacao.removeAttribute("disabled");
-        
-                
+        validacao.style.color = "green";
+
     } else {
         imgValidacao.style.backgroundColor = "#ffffff";
         validacao.innerText = "Campo obrigatorio com no minimo um endereço válido ex: Http ou Https";
-        validacao.style.color="red";
-        btnValidacao.setAttribute("disabled", true);        
-    }   
+        validacao.style.color = "red";
+
+    }
 })
 
-descricaoCidadeValidacao.addEventListener("keyup", function(){
-    let validacao = document.getElementById("descricaoSmall");
-    if (descricaoCidadeValidacao.value.length >= 4) {
-        validacao.innerText = "Ok!";
-        validacao.style.color="green";
-        btnValidacao.style.backgroundColor="#3d9db3" ;
-        btnValidacao.style.cursor = "pointer";
-        btnValidacao.removeAttribute("disabled");
-        recarregarCampos.removeAttribute("disabled", true); 
-        recarregarCampos.style.backgroundColor="#3d9db3" ;      
-       
-       
-    } else {
-        descricaoCidadeValidacao.style.backgroundColor = "#ffffff";
-        validacao.innerText = "Campo obrigatorio com 4 letras no minimo";
-        validacao.style.color="red"; 
-        btnValidacao.style.backgroundColor="gray" ; 
-                          
-    }    
-})
+// validando o form por completo;
 
+function validateForm() {
 
-//setInterval(recarregarCampo, 10000);
-
-function recarregarCampo() {
-
-if (cidadeValidacao.value.length >=1 && pontoTuristicoValidacao.value.length >=1 && imgValidacao.value.length>=1 && descricaoCidadeValidacao.value.length>=1){
-    cidadeValidacao.value="";
-    pontoTuristicoValidacao.value="";
-    imgValidacao.value="";
-    descricaoCidadeValidacao.value="";
+    if (!(cidadeValidacao.value.length >= 4 && pontoTuristicoValidacao.value.length >= 4 && imgValidacao.value.length >= 4 && descricaoCidadeValidacao.value.length >= 4)) {
+        alert("Name must be filled out");
+        return false;
     }
 }
-  
- 
-
-
 
