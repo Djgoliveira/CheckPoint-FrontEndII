36f7edce41b09406c41d63bf6cdfaf1848543f33
GustavoFlorenci0 <florencio152@gmail.com>
2023-03-15 21:12:11 -0300

    Ajustando validaçoes

--- javaScript/codigo.js
+++ javaScript/codigo.js
@@ -6,36 +6,62 @@ elementoPai.classList.add('banner-cards');
 
 // Criando um construtor para utilizar o cadastro.
 
-function PontosTuristicos(imageUrl, cidade, pontoTuristico, descricao) {
-    this.imageUrl = imageUrl;
-    this.cidade = cidade;
-    this.pontoTuristico = pontoTuristico;
-    this.descricao = descricao;
+class PontosTuristicos {
+    constructor(imageUrl, cidade, pontoTuristico, descricao) {
+        this.imageUrl = imageUrl;
+        this.cidade = cidade;
+        this.pontoTuristico = pontoTuristico;
+        this.descricao = descricao;
+    }
 }
 
 // criando a lista.
 
 let listaPontoTuristicos = [];
 
+// variaveis de uso comum;
+
+const imgUrl = document.getElementById("url_cad");
+const nomeCidade = document.getElementById("nome_cad");
+const pontoTuristico = document.getElementById("NomeLocal_cad");
+const descricaoCidade = document.getElementById("descricao_cad");
+const btnSmall = document.getElementById("btnSmall");
+let validacaoImg = document.getElementById("imgSmall");
+let validacaoPT = document.getElementById("localSmall");
+let validacaoCidade = document.getElementById("smallCidade");
+
 // criando a função de cadastrar;
 
 function Cadrastrar() {
 
-    const imgUrl = document.getElementById("url_cad");
-    const nomeCidade = document.getElementById("nome_cad");
-    const pontoTuristico = document.getElementById("NomeLocal_cad");
-    const descricaoCidade = document.getElementById("descricao_cad");
-
-    const n = new PontosTuristicos(imgUrl.value, nomeCidade.value, pontoTuristico.value, descricaoCidade.value);
-    if (nomeCidade.value && pontoTuristico.value && imgUrl.value && descricaoCidade.value) {
-        listaPontoTuristicos.push(n);
-        rendenizarCards();        
+    if (nomeCidade.value.length >= 4 && pontoTuristico.value.length >= 4 && imgUrl.value.length >= 4 && descricaoCidade.value.length >= 4) {
+        btnValidade = true;
+    } else {
+        btnValidade = false;
     }
-    imgUrl.value = "";
-    nomeCidade.value = "";
-    pontoTuristico.value = "";
-    descricaoCidade.value = "";
-    
+
+    console.log(btnValidade);
+    if (btnValidade == true) {
+        const n = new PontosTuristicos(imgUrl.value, nomeCidade.value, pontoTuristico.value, descricaoCidade.value);
+        if (nomeCidade.value && pontoTuristico.value && imgUrl.value && descricaoCidade.value) {
+            listaPontoTuristicos.push(n);
+            rendenizarCards();
+        }
+        imgUrl.value = "";
+        nomeCidade.value = "";
+        pontoTuristico.value = "";
+        descricaoCidade.value = "";
+        btnSmall.innerText = "";
+        validacaoImg.innerText = "";
+        validacaoCidade.innerText = "";
+        validacaoPT.innerText = "";
+
+    } else {
+        btnSmall.innerText = "Falta cadastrar informações";
+        btnSmall.style.color = "red";
+    }
+
+
 };
 
 // criando a função para renderizar os cards;
@@ -57,3 +83,44 @@ function rendenizarCards() {
         elementoPai.appendChild(novaDiv);
     }
 }
+
+// criando as validações
+
+nomeCidade.addEventListener("keyup", function () {
+    if (nomeCidade.value.length >= 4) {
+        validacaoCidade.innerText = "Ok!";
+        validacaoCidade.style.color = "green";
+
+    } else {
+        nomeCidade.style.backgroundColor = "#ffffff";
+        validacaoCidade.innerText = "Campo obrigatorio com 4 letras no minimo";
+        validacaoCidade.style.color = "red";
+    }
+})
+
+pontoTuristico.addEventListener("keyup", function () {
+
+    if (pontoTuristico.value.length >= 4) {
+        validacaoPT.innerText = "Ok!";
+        validacaoPT.style.color = "green";
+
+    } else {
+        pontoTuristico.style.backgroundColor = "#ffffff";
+        validacaoPT.innerText = "Campo obrigatorio com 4 letras no minimo";
+        validacaoPT.style.color = "red";
+
+    }
+})
+
+imgUrl.addEventListener("keyup", function () {
+
+    if (imgUrl.value.length >= 10) {
+        validacaoImg.innerText = "Ok!";
+        validacaoImg.style.color = "green";
+
+    } else {
+        imgUrl.style.backgroundColor = "#ffffff";
+        validacaoImg.innerText = "Campo obrigatorio com no minimo um endereço válido ex: Http ou Https";
+        validacaoImg.style.color = "red";
+    }
+})
